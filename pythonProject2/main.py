0a2ff707b9c994058068a2031d03fa23017402de
ilonasemencha <94219792+ilonasemencha@users.noreply.github.com>
2021-11-13 19:24:48 +0200

    Homework 1

---
+++ pythonProject2/main.py
@@ -0,0 +1,20 @@
+# Task 1
+print("Hello world")
+# Task 2
+print("Hello world", 2021, "Ilona")
+# Task 3
+length = float(input("length="))
+width = float(input("width="))
+print(length * width)
+# Task 4
+x = float(input("x="))
+y = float(input("y="))
+print("sum", x + y)
+print("product", x * y)
+print("difference", x - y)
+print("quotient", x / y)
+# Task 5
+radius = float(input("radius="))
+print("diameter", radius * 2)
+print("circumference", radius * 2 * 3.14159)
+print("area", radius * radius * 3.14159)
